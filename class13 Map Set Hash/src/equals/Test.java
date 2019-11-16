bca20c37af7239b5af813b7a3e12e447b3af442c
maolintuntun <diyeqx@gmail.com>
2019-11-16 01:19:53 -0500

    update

--- class13 Map Set Hash/src/equals/Test.java
+++ class13 Map Set Hash/src/equals/Test.java
@@ -3,7 +3,7 @@ package equals;
 public class Test {
 	public static void main(String[] args) {
 		String x = "abc";
-		String y = "abc";
+		String y = new String("abc");
 		System.out.println(x==y);
 	}
 
--- class13 Map Set Hash/src/equals/Test2.java
+++ class13 Map Set Hash/src/equals/Test2.java
@@ -1,5 +1,47 @@
 package equals;
 
-public class Test2 {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Test2 {
+	public static void main(String[] args) {
+		lisa o1 = new lisa(3,7);
+		lisa o2 = new lisa(3,7);
+		System.out.println(o1.equals(o2));
+		
+		Map<lisa, Integer> map = new HashMap<>();
+		map.put(o1,1);
+		map.put(o2,3);
+		System.out.println(map.size());
+		
+	}
+		
+	
+
+}
+
+class lisa{
+	public int x;
+	public int y;
+	
+	public lisa(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public boolean equals(Object other) {
+		if(!(other instanceof lisa)) { // 如果other根本就不是lisa这个类型 就没必要比了
+			return false;
+		}
+		lisa o = (lisa) other; //否则把other cast成lisa 
+		return x == o.x && y == o.y; //because we want to compare 内容, 所以我们比较x和y两个参数是否相等so let the arguments compared.
+		
+	}
+	
+	@Override
+	public int hashCode(){
+	
+	return 31*x+y; // kx + y =kx2 + y2
+}
 }
